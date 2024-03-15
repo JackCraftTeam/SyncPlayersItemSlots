815ad5f4a802ab884ec1c038a59a00cba4ae695d
Jack Lei <3486688394@qq.com>
2024-03-16 00:22:19 +0800

    refactor: 移除不必要注释

--- src/main/java/cn/jackcraft/syncplayersitemslots/Listeners/PlayerJoinListener.java
+++ src/main/java/cn/jackcraft/syncplayersitemslots/Listeners/PlayerJoinListener.java
@@ -14,15 +14,11 @@ public class PlayerJoinListener implements Listener {
     @EventHandler(priority = EventPriority.LOW)
     public void onPlayerJoin(PlayerJoinEvent event) {
         Map<Integer, Integer> slotCounts = new HashMap<>();
-        // 获取玩家
         Player player = event.getPlayer();
-        // 遍历所有在线玩家
         for (Player p : Bukkit.getOnlinePlayers()) {
             int slot = p.getInventory().getHeldItemSlot();
-            // 统计每个栏位的出现频率
             slotCounts.put(slot, slotCounts.getOrDefault(slot, 0) + 1);
         }
-        // 找到出现频率最高的栏位
         int mostCommonSlot = -1;
         int maxCount = 0;
         for (Map.Entry<Integer, Integer> entry : slotCounts.entrySet()) {
@@ -34,7 +30,6 @@ public class PlayerJoinListener implements Listener {
             }
         }
         if (mostCommonSlot != -1) {
-            // 设置槽位
             player.getInventory().setHeldItemSlot(mostCommonSlot);
         }
     }
