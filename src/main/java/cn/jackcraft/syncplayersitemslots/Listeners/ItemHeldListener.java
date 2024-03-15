fbf005a876d8497919653c92005dc26380cbf113
Jack Lei <3486688394@qq.com>
2024-03-15 23:48:10 +0800

    feat: 支持同步玩家物品栏位

---
+++ src/main/java/cn/jackcraft/syncplayersitemslots/Listeners/ItemHeldListener.java
@@ -0,0 +1,22 @@
+package cn.jackcraft.syncplayersitemslots.Listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+
+public class ItemHeldListener implements Listener {
+    @EventHandler(priority = EventPriority.LOW)
+    public void onPlayerItemHeld(PlayerItemHeldEvent event) {
+        Player player = event.getPlayer();
+        Player[] players = Bukkit.getServer().getOnlinePlayers().toArray(new Player[0]);
+        int currentSlot = event.getNewSlot();
+        for (Player p : players){
+            if (!p.equals(player)){
+                p.getInventory().setHeldItemSlot(currentSlot);
+            }
+        }
+    }
+}
--- src/main/java/cn/jackcraft/syncplayersitemslots/SyncPlayersItemSlots.java
+++ src/main/java/cn/jackcraft/syncplayersitemslots/SyncPlayersItemSlots.java
@@ -1,17 +1,15 @@
 package cn.jackcraft.syncplayersitemslots;
 
+import cn.jackcraft.syncplayersitemslots.Listeners.ItemHeldListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SyncPlayersItemSlots extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(new ItemHeldListener(), this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
