209ae56c04ff0bf71f8d78d2eb42fe50ca6de6c5
Jack Lei <3486688394@qq.com>
2024-03-16 00:18:35 +0800

    feat: 在禁用时取消注册

--- src/main/java/cn/jackcraft/syncplayersitemslots/SyncPlayersItemSlots.java
+++ src/main/java/cn/jackcraft/syncplayersitemslots/SyncPlayersItemSlots.java
@@ -1,6 +1,7 @@
 package cn.jackcraft.syncplayersitemslots;
 
 import cn.jackcraft.syncplayersitemslots.Listeners.ItemHeldListener;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SyncPlayersItemSlots extends JavaPlugin {
@@ -11,5 +12,6 @@ public final class SyncPlayersItemSlots extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        HandlerList.unregisterAll(this);
     }
 }
